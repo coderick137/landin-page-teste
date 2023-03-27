c1fcd8c81431d6bd63b772fa4e61b1302b158333
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-26 23:22:05 -0300

    altera cor de fundo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Content } from "./components/Content";
 import { Header } from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-[#1C2C44] ">
+    <main className="h-screen bg-[#1B2033] ">
       <Header />
+      <Content />
     </main>
   )
 }
